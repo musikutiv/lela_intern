02a2dac34d2e5dc761c1ea26a791701858c93d34
Lela <lela@Tobiass-iMac.local>
2020-06-24 16:01:10 +0200

    shared

--- trial_script.R
+++ trial_script.R
@@ -1,7 +1,7 @@
 
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager", type=source())
+  install.packages("BiocManager", type='source')
 BiocManager::install(version = "3.11")
 
 library(BiocManager)
